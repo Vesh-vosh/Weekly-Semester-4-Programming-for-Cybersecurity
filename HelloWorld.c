2bcd23e4aba34a225e01863909f7ff1527b3f5d7
Vesh <sarveshysharma33@gmail.com>
2026-03-28 11:40:12 +0545

    1st commit

---
+++ HelloWorld.c
@@ -0,0 +1,6 @@
+ /* Print a message on the screen */                                               
+#include<stdio.h> 
+ int main() {     /* code */ 
+    printf("Hello World! \n");     
+    return 0; 
+}   
