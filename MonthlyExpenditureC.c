ebd741d4ba375b6b0b8d7cf1f82d2d1cbfc19008
Vesh <sarveshysharma33@gmail.com>
2026-04-01 12:01:06 +0545

    MonthlyExpenditureC

--- MonthlyExpenditureC.c
+++ MonthlyExpenditureC.c
@@ -4,10 +4,12 @@ int main() {
     float foodExpenses = 230.0;
 float leisureExpenses = 70.0;
 float clothesExpenses = 60.0;
+float accommodationExpenses = 500.0;
+float travelExpenses = 60.0;
 
 float totalSpent;
 
-totalSpent = foodExpenses + leisureExpenses + clothesExpenses;
+totalSpent = foodExpenses + leisureExpenses + clothesExpenses + accommodationExpenses + travelExpenses;
 
 
 printf("The total expenditure this month was %.2f\n\n", totalSpent);
