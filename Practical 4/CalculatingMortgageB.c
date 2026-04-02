febf865c94eeda135d3917896bcc982c8dbf85b0
Vesh <sarveshysharma33@gmail.com>
2026-04-02 17:54:48 +0545

    CalculatingMortgageB

--- Practical 4/CalculatingMortgageB.c
+++ Practical 4/CalculatingMortgageB.c
@@ -1,32 +1,25 @@
-/* CalculateMortgageB.c  
-Practical 4, Part 1 (b) 
-@author Your Name */
 
 #include <stdio.h>
 
-// Function declarations
 double largerSalary(double salary1, double salary2);
 double smallerSalary(double salary1, double salary2);
 
 int main() {
-    // Declare variables
+   
     double salary1, salary2, mortgage;
 
-    // Input salaries
+ 
     printf("Enter two salaries separated by a space:\n");
     scanf("%lf %lf", &salary1, &salary2);
 
-    // Calculate mortgage using functions
     mortgage = largerSalary(salary1, salary2) * 3;
     mortgage += smallerSalary(salary1, salary2);
 
-    // Display result
-    printf("The maximum size of mortgage is: £ %.2lf\n", mortgage);
+    printf("The maximum size of mortgage is: %.2lf\n", mortgage);
 
     return 0;
 }
 
-// Function stubs
 double largerSalary(double salary1, double salary2) {
     return 1;
 }
